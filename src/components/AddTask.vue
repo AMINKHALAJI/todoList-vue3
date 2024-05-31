a570d0ca95d9e2df0f64f2310684bb489444128c
Amin <amin19.kntu@gmail.com>
2024-05-31 15:19:05 +0200

    static part of the app is done

---
+++ src/components/AddTask.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full"  >
+        <form action="#" class="py-10 w-full flex  justify-around ">
+            <input type="text" class="py-2 px-4 rounded-lg w-3/5" name="" id="" placeholder="Enter a new task">
+            <button class= " py-2 px-4 rounded-md bg-cyan-50 text-teal-600">Add Task</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+  
+    <div
+      class="flex w-3/5 mt-5  py-3 px-4 rounded justify-between items-center bg-cyan-50 text-black"
+    >
+      <h4>going out with friends</h4>
+      <button class="text-red-600 ">x</button>
+    </div>
+    <div
+      class="flex w-3/5 mt-5  py-3 px-4 rounded justify-between items-center bg-cyan-50 text-black"
+    >
+      <h4>buy the groceries</h4>
+      <button class="text-red-600 ">x</button>
+    </div>
+    <div
+      class="flex w-3/5 mt-5  py-3 px-4 rounded justify-between items-center bg-cyan-50 text-black"
+    >
+      <h4>going out with friends</h4>
+      <button class="text-red-600 ">x</button>
+    </div>
+  
+</template>
+
+<script setup></script>
+
+<style scoped></style>
