4b522d1e90c0cccddb8960edaf73959ad64e657e
Amin <amin19.kntu@gmail.com>
2024-05-31 14:17:47 +0200

    init step

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from"./components/Header.vue"
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen bg-neutral-300">
+
+  
+  <div class=" h-[40em] w-2/5 flex items-start   justify-center bg-cyan-800 rounded-2xl ">
+
+    <Header/>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl text-cyan-50 pt-5">Todo List</h1>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style>
+
+</style>
